dacb453e4f175b321b1eb81ac4caf34352278552
Wheheo Hu <wheheohu@outlook.com>
2023-03-18 23:21:00 +0800

    test import

--- source_import.py
+++ source_import.py
@@ -14,8 +14,11 @@ def source_import(DID:str):
         exit()
     dtg_path=DTG_Path(DID_8=DID,volume_name='RAID6_230201',volume_type='RAID')
 
+    SOURCE_PATH=dtg_path.get_source_path()
+    #SOURCE_PATH="/Volumes/DTG_SSD_WD03/DAY_005_20230318/Source"
     
-    reel_list=[f for f in os.scandir(dtg_path.get_source_path()) if f.is_dir()]
+    
+    reel_list=[f for f in os.scandir(SOURCE_PATH) if f.is_dir()]
     reel_clip_path_list=[Reel_Importer(f).get_source_clip_path() for f in reel_list]
     LOCAL_RESOLVE=Bmd()
 
